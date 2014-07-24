fb8faa4f8d583e7f599609ec03ccf3251ddf448b
eliu9 <ericaliu9715@gmail.com>
2014-07-24 11:14:57 -0700

    edited Duplicate Checker, new method to identify which lists duplicate resources show up in

--- DuplicatesChecker.py
+++ DuplicatesChecker.py
@@ -31,28 +31,51 @@ list_of_titles(hl, hlt)
 list_of_titles(sen, sent)
 list_of_titles(cp, cpt)
 
-#to check to see if list_of_titles() works
-print(len(wst))
-print(len(hlt))
-print(len(sent))
-print(len(cpt))
+duplicates = []
+inventories = [cpt, hlt, sent, wst]
 
 def dupcheck(list1, list2, list3):
     for value in list1:
         if value in list2 and value not in list3:
             list3.append(value)
 
-duplicates = []
+#still need to find a more efficient way to go through lists, or make a more efficient method to do this
+#dupcheck(sent, wst, duplicates)
+#dupcheck(sent, hlt, duplicates)
+#dupcheck(sent, cpt, duplicates)
+#dupcheck(wst, hlt, duplicates)
+#dupcheck(wst, cpt, duplicates)
+#dupcheck(hlt, cpt, duplicates)
+#method below pretty much just does ^this
+def multiple_dupcheck(l, dl):
+    list1 = []
+    for value in l:
+        list1.append(value)
+        #have to do it ^this way so that list l stays intact even after the method is over
+    while len(list1) > 1:
+        list2 = list1[0]
+        list1.remove(list1[0])
+        for value in list1:
+            dupcheck(list2, value, dl)
 
-#need to find a more efficient way to go through lists, or make a more efficient method to do this
-#also need to figure out how to indicate which lists the duplicates show up in
-dupcheck(sent, wst, duplicates)
-dupcheck(sent, hlt, duplicates)
-dupcheck(sent, cpt, duplicates)
-dupcheck(wst, hlt, duplicates)
-dupcheck(wst, cpt, duplicates)
-dupcheck(hlt, cpt, duplicates)
+#found a way to indicate which lists the duplicates show up in, though it's a bit convoluted
+#prints a list of all duplicate resources along with a four digit id consisting of 1's and 0's
+#each digit corresponds with an inventory: [C4P, High Level, SEN, Workshop]
+#a 1 indicates that the resource is present in the inventory, a 0 indicates that it is not
+def present_in_lists(resources, lists):
+    ids = []
+    for value in resources:
+        itemid = str()
+        for l in lists:
+            if value in l:
+                itemid = itemid + "1"
+            else:
+                itemid = itemid + "0"
+        ids.append(value + " : " + itemid)
+    return ids
 
-print(duplicates)
-print("done")
+#plan on trying to figure out a way to integrate present_in_lists and the duplicate checker into one method
 
+multiple_dupcheck(inventories, duplicates)
+print(present_in_lists(duplicates, inventories))
+print(len(duplicates))
