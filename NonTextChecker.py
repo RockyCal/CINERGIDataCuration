e9d4c7833a8f849b1b2caf93dc7df6732ac3f638
eliu9 <ericaliu9715@gmail.com>
2014-08-13 09:48:26 -0700

    edited program, now shows index of non-text symbols

--- NonTextChecker.py
+++ NonTextChecker.py
@@ -1,6 +1,5 @@
-#checks for non-text symbols in resource titles, descriptions, and abstracts
-
 import urllib.request
+import time
 from openpyxl import load_workbook
 from bs4 import BeautifulSoup
 
@@ -9,20 +8,20 @@ senx = load_workbook("detailsSEN.xlsx")
 sen = senx.active
 senlinks = []
 
-#cpx = load_workbook("detailsC4P.xlsx")
-#cp = cpx.active
-#cplinks = []
+cpx = load_workbook("detailsC4P.xlsx")
+cp = cpx.active
+cplinks = []
 
-#hlx = load_workbook("detailsHL.xlsx")
-#hl = hlx.active
-#hllinks = []
+hlx = load_workbook("detailsHL.xlsx")
+hl = hlx.active
+hllinks = []
 
-#wsx = load_workbook("detailsWS.xlsx")
-#ws = wsx.active
-#wslinks = []
+wsx = load_workbook("detailsWS.xlsx")
+ws = wsx.active
+wslinks = []
 
-#inventories = [cp, hl, sen, ws]
-#linklists = [cplinks, hllinks, senlinks, wslinks]
+workbooks = [cp, hl, sen, ws]
+urllists = [cplinks, hllinks, senlinks, wslinks]
 
 URL = "A"
 start = 1
@@ -32,9 +31,7 @@ def list_of_links(workbook, llist):
         for cell in row:
             llist.append(cell.value)
 
-#returns list of resources with non-text symbols and the section that the symbol appears in
-#if a location appears twice (e.g. <title>-abstract-abstract) it means that a non-text symbol
-#appears twice in that location
+#returns list of non-text symbols
 def multiplefilechecker(llist):
     newlist = []
     for link in llist:
@@ -42,6 +39,7 @@ def multiplefilechecker(llist):
         add = filechecker(soup)
         if len(add) > 1:
             newlist.append(add)
+    print(len(newlist), " resources containing at least one non-text symbol")
     return newlist
 
 def filechecker(soup):
@@ -49,9 +47,10 @@ def filechecker(soup):
     newstring = str(soup.find("td"))
     for item in fields:
         string = str(item)
-        count = 0
         if fields.index(item) != 2:
+            i = -5
             for char in string:
+                i += 1
                 if ord(char) > 126:
                     if fields.index(item) == 0:
                         newstring += "-title"
@@ -59,9 +58,36 @@ def filechecker(soup):
                         newstring += "-description"
                     elif fields.index(item) == 3:
                         newstring += "-abstract"
+                    newstring += str(i)                
     if len(newstring) == len(str(soup.find("td"))):
         newstring = ""
+    newstring = newstring.replace("<td>", "")
+    newstring = newstring.replace("</td>", "")
     return newstring
 
-list_of_links(sen, senlinks)
-print(multiplefilechecker(senlinks))
+start_time = time.time()
+
+count = 0
+while count < 4:
+    list_of_links(workbooks[count], urllists[count])
+    count += 1
+
+print("Non-text symbols in C4P:")
+for item in multiplefilechecker(cplinks):
+    print(item)
+print()
+print("Non-text symbols in HLI:")
+for item in multiplefilechecker(hllinks):
+    print(item)
+print()
+print("Non-text symbols in SEN:")
+for item in multiplefilechecker(senlinks):
+    print(item)
+print()
+print("Non-text symbols in WSI:")
+for item in multiplefilechecker(wslinks):
+    print(item)
+print()
+
+elapsed_time = time.time() - start_time
+print(elapsed_time)
